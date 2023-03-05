fe4863f044b3c3c1069ebe94860119461b6eb6af
siraj <sirajbhutta50@gmail.com>
2023-03-05 22:13:40 +0500

    🌎 docs: landing page design

--- src/Component/demo/landing/main.jsx
+++ src/Component/demo/landing/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import styled from 'styled-components';
-import {VscGithub,VscGlobe,VscTwitter } from "react-icons/vsc";
-import { Button,  design, component, library } from '../App'
+import { VscGithub, VscGlobe, VscTwitter } from "react-icons/vsc";
 import { typography,space,color,flexbox,layout,border } from 'studio-props-system'
+
+import { Button,  design, component, library } from '../App'
 
 /*
 todo: landing page  
