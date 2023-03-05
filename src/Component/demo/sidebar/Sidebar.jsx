90f4cb7fed60bfc41ba24c4f68a8078bf8dcf1e1
siraj <sirajbhutta50@gmail.com>
2023-03-05 22:07:44 +0500

    ❌ fix: github link

--- src/Component/demo/sidebar/Sidebar.jsx
+++ src/Component/demo/sidebar/Sidebar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { VscGithub,VscNote,VscLibrary } from "react-icons/vsc";
 import { MdOutlineDashboard } from "react-icons/md";
-import { Section, Flex, Text, } from '../App'
 import { Link } from 'react-router-dom';
+
+import { Section, Flex, Text, } from '../App'
 
 const Sidebar = () => {
   const SideBartop = {width: '55px',position: 'fixed',};
-  const LinkColor = {color: 'rgba(158, 158, 158, 1)',};
+  const LinkColor = { color: 'rgba(158, 158, 158, 1)', };
   return (
     <>
       <Section bg='bgc' darktext style={SideBartop}>
@@ -16,7 +17,7 @@ const Sidebar = () => {
             <Text Start='3' Top='5'><Link style={LinkColor} to='intro'><VscNote size={24}/></Link></Text>
             <Text Start='3' Top='3'><Link style={LinkColor} to='library'><VscLibrary size={24}/></Link></Text>
           </Section>
-          <Text Bottom='3'  Start='3' smallHeading><Link style={LinkColor}><VscGithub /></Link></Text>
+          <Text Bottom='3'  Start='3' smallHeading><Link to='https://github.com/engsiraj/studio' style={LinkColor}><VscGithub /></Link></Text>
           </Flex>
       </Section>
     </>
