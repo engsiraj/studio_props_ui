7c202c9e38f2d5737880f8dd84f13d8ab4ab94d1
siraj <sirajbhutta50@gmail.com>
2023-03-05 22:11:50 +0500

    🚧 fix: cards grid

--- src/Component/demo/layout/main.jsx
+++ src/Component/demo/layout/main.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
-import { typography,space,color,flexbox,layout,border } from 'studio-props-system'
+import { typography,space,color,grid,layout,border } from 'studio-props-system'
 
 /*
 todo: Components page 
 ------------
 ? sections
 ------------
+ repeat(auto-fill, minmax(260px, 1fr))
+space 
+"0.25rem", "0.5rem", "0.75rem", "1.25rem", "1.5rem", "2rem"
+*/
+
 const device = {
   phone: `(max-width: 37.5rem)`,
   tab: `(min-width: 56.25rem)`,
   laptop: `(min-width: 75rem)`,
   desktop: `(min-width: 112.5rem)`,
 };
-
-space 
-"0.25rem", "0.5rem", "0.75rem", "1.25rem", "1.5rem", "2rem"
-*/
 
 const Wrapper = styled.div`
   padding-top: 5%;
@@ -27,14 +28,25 @@ const Wrapper = styled.div`
 const Section = styled.div`
   ${layout}
   ${color}
+   @media ${device.phone} {
+    width:100%;}
 `;
 const Box = styled.div`
-  ${flexbox}
+  ${grid}
 `;
 const Text = styled.p`
   ${typography}
   ${space}
   ${color}
+`;
+const Card = styled.div`
+  ${typography}
+  ${border}
+  ${space}
+  ${color}
+  ${layout}
+  @media ${device.phone} {
+    width:100%;}
 `;
 
 const Components = () => {
@@ -43,15 +55,11 @@ const Components = () => {
       <Section width='70%' color='Dark100'>
         <Text fontSize='lgHeading'  fontWeight='600' pb='18px' >Library</Text>
         <Text pb='24px' fontSize='text' color='Gray500'> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat, accusamus.</Text>
-        <Box justifyContent='space-between' >
-          <CardSec title='grid' desc='this is other type '/>
+        <Box templateColumn='repeat(auto-fill, minmax(260px, 1fr));' gridGap='.4rem'>
+          <CardSec title='color' desc='props color system'/>
+          <CardSec title='grid' desc='other thing'/>
           <CardSec title='grid' desc='description'/>
           <CardSec title='grid' desc='description'/>
-          <CardSec title='grid' desc='description'/>
-        </Box>
-        <Box justifyContent='space-between' >
-          <CardSec title='grid' desc='description'/>
-          <CardSec title='grid' desc='something better'/>
           <CardSec title='grid' desc='description'/>
           <CardSec title='grid' desc='description'/>
         </Box>
@@ -71,16 +79,10 @@ const Components = () => {
   )
 }
 
-const Card = styled.div`
-  ${border}
-  ${space}
-  ${color}
-  ${layout}
-`;
 const CardSec = p => {
   return (
     <>
-     <Card corner='.75rem' p='10px' backgroundColor='Dark400' width='24%' mb='12px' border='1px solid rgba(100, 100, 100, 0.1);'>
+     <Card corner='.75rem' p='10px' backgroundColor='Dark400'  border='1px solid rgba(100, 100, 100, 0.1);'>
         <Text color='Dark100' pb='6px' fontSize='regualar' fontWeight='500'>{p.title}</Text>
             <Text color='Gray500'>{p.desc}</Text>
       </Card>
