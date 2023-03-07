fc9c9d0e48177b256a1eb214de484608432a24e8
siraj <sirajbhutta50@gmail.com>
2023-03-07 23:51:20 +0500

    🌐 webapage: data

--- src/Component/demo/intro/data.jsx
+++ src/Component/demo/intro/data.jsx
@@ -1,2 +1,5 @@
-export const install ='npm install studio-props-system'
+
+
+// code snpies
+export const install = 'npm install studio-props-system'
 export const usage = "import styled from 'styled-components';\nimport {typography,color,space} from 'studio-props-system';\n\nconst Text = styled.p`\n${typography}\n${space}\n${color}`\n\nconst Component = () => {\nreturn (\n<Text fontSize='lgHeading' fontWeight='600' color='Dark100' pb='18px' >studio props system</Text>\n)}"
