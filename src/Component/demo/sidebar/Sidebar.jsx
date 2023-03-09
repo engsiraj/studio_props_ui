251eb34d4d20cb5e5517154b96b8102a5a448fc2
siraj <sirajbhutta50@gmail.com>
2023-03-10 00:14:07 +0500

    💅 lib: design system

--- src/Component/demo/sidebar/Sidebar.jsx
+++ src/Component/demo/sidebar/Sidebar.jsx
@@ -2,24 +2,52 @@ import React from 'react'
 import { VscGithub,VscNote,VscLibrary } from "react-icons/vsc";
 import { MdOutlineDashboard } from "react-icons/md";
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { typography, space, color, layout, border,  flexbox } from 'studio-props-system';
 
-import { Section, Flex, Text, } from '../App'
+
+
+const Text = styled.p`
+  ${typography}
+  ${space}
+  ${color}
+`;
+const CardLink = styled(Link)`
+  ${typography}
+  ${color}
+  cursor: pointer;
+  text-decoration: none;
+  :hover{
+    opacity:0.6;
+    transition: ease-in 450ms;
+    transition: ease-out 300ms;
+  }
+`;
+const Card = styled.div`
+  ${typography}
+  ${border}
+  ${space}
+  ${color}
+`;
+const Box = styled.div`
+   ${flexbox}
+   ${layout}
+`;
 
 const Sidebar = () => {
-  const SideBartop = {width: '55px',position: 'fixed',};
-  const LinkColor = { color: 'rgba(158, 158, 158, 1)', };
+  const SideBartop = {position: 'fixed'};
   return (
     <>
-      <Section bg='bgc' darktext style={SideBartop}>
-        <Flex flexDirection='column' justifyContent='space-between' height='100vh' >
-          <Section>
-            <Text Start='2' Top='3' ><Link style={LinkColor} to='/'><MdOutlineDashboard size={36}/></Link></Text>
-            <Text Start='3' Top='5'><Link style={LinkColor} to='intro'><VscNote size={24}/></Link></Text>
-            <Text Start='3' Top='3'><Link style={LinkColor} to='library'><VscLibrary size={24}/></Link></Text>
-          </Section>
-          <Text Bottom='3'  Start='3' smallHeading><Link to='https://github.com/engsiraj/studio' style={LinkColor}><VscGithub /></Link></Text>
-          </Flex>
-      </Section>
+      <Card backgroundColor='Dark400' width='55px' style={SideBartop}>
+        <Box flexDirection='column' justifyContent='space-between' height='100vh'>
+          <Card>
+            <Text pt='20px' px='9px'><CardLink color='Gray600' to='/'><MdOutlineDashboard size={36}/></CardLink></Text>
+            <Text px='14px' pt='18px'><CardLink color='Gray600' to='intro'><VscNote size={24}/></CardLink></Text>
+            <Text px='14px' pt='12px'><CardLink color='Gray600' to='library'><VscLibrary size={24}/></CardLink></Text>
+          </Card>
+          <Text px='14px'   pb='18px' smallHeading><CardLink color='Gray600' to='https://github.com/engsiraj/studio' ><VscGithub size={24}/></CardLink></Text>
+          </Box>
+      </Card>
     </>
   )
 }
